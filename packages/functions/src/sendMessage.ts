6be0a9e7d12a521a52d92f0a9cb8bc8d637cfed9
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-05-02 23:37:58 -0400

    Initial commit

---
+++ packages/functions/src/sendMessage.ts
@@ -0,0 +1,48 @@
+import { DynamoDB, ApiGatewayManagementApi } from "aws-sdk";
+import { Table } from "sst/node/table";
+
+const TableName = Table.Connections.tableName;
+const dynamoDb = new DynamoDB.DocumentClient();
+
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+export const main: APIGatewayProxyHandler = async (event) => {
+  const messageData = JSON.parse(event.body!).data;
+  const { stage, domainName } = event.requestContext;
+
+  const apiG = new ApiGatewayManagementApi({
+    endpoint: `${domainName}/${stage}`,
+  });
+
+  const postToConnection = async function ({ id }: { id: string }) {
+    try {
+      // Send the message to the given client
+      await apiG
+        .postToConnection({ ConnectionId: id, Data: messageData })
+        .promise();
+    } catch (e: any) {
+      if (e.statusCode === 410) {
+        // Remove stale connections
+        await dynamoDb.delete({ TableName, Key: { id } }).promise();
+      }
+    }
+  };
+
+  let connections: DynamoDB.DocumentClient.ScanOutput | undefined;
+
+  do {
+    // Get all the connections
+    connections = await dynamoDb
+      .scan({
+        TableName,
+        ProjectionExpression: "id",
+        ExclusiveStartKey: connections?.LastEvaluatedKey,
+      })
+      .promise();
+
+    // Iterate through all the connections
+    await Promise.all(connections.Items!.map(postToConnection as any));
+  } while (typeof connections.LastEvaluatedKey !== "undefined");
+
+  return { statusCode: 200, body: "Message sent" };
+};
